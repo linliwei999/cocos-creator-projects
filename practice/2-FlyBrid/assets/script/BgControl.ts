799333b4d83e8b45b611160318ee2125e18c61e5
linliwei <linliwei999@foxmail.com>
2023-12-06 23:02:25 +0800

    commit 添加点击小鸟飞翔逻辑

---
+++ practice/2-FlyBrid/assets/script/BgControl.ts
@@ -0,0 +1,45 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+import BridControl from "./BridControl";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class BgControl extends cc.Component {
+    //速度
+    @property
+    speed: number = 4;
+    //宽度
+    @property
+    width: number = 288;
+
+    //小鸟
+    @property(BridControl)
+    brid: BridControl = null;
+
+    start () {
+        // 点击监听
+        // this.node.on 
+        for(let bg of this.node.children){
+            bg.on(cc.Node.EventType.TOUCH_START, ()=>{
+                this.brid.fly();
+            });
+        }
+
+    }
+
+    update (dt) {
+        //移动
+        for(let bg of this.node.children){
+            bg.x -= this.speed * dt;
+            if(bg.x < -this.width){
+                bg.x += this.width *2 -10;
+            }
+        }
+    }
+}
--- practice/2-FlyBrid/assets/script/BridControl.ts
+++ practice/2-FlyBrid/assets/script/BridControl.ts
@@ -17,25 +17,25 @@ export default class BridControl extends cc.Component {
         //开启物理引擎
         cc.director.getPhysicsManager().enabled = true;
         // 打出一条射线
-        let results = cc.director.getPhysicsManager().rayCast(this.node.getPosition(), cc.v2(this.node.x, this.node.y + 100), cc.RayCastType.Closest);
-        for(let i = 0; i < results.length; i++){
-            let res = results[i];
-            //射线碰到的碰撞器
-            res.collider;
-            //触碰到的点
-            res.point;
-            //碰到的法线
-            res.normal;
-        }
+        // let results = cc.director.getPhysicsManager().rayCast(this.node.getPosition(), cc.v2(this.node.x, this.node.y + 100), cc.RayCastType.Closest);
+        // for(let i = 0; i < results.length; i++){
+        //     let res = results[i];
+        //     //射线碰到的碰撞器
+        //     res.collider;
+        //     //触碰到的点
+        //     res.point;
+        //     //碰到的法线
+        //     res.normal;
+        // }
     }
 
     start () {
-        let rbody = this.getComponent(cc.RigidBody);
-        //力
-        // rbody.applyForce(cc.v2(1000, 0), cc.v2(0, 0), true);
-        rbody.applyForceToCenter(cc.v2(1000, 0), true);
-        //速度 每秒多少像素
-        rbody.linearVelocity = cc.v2(50, 0);
+        // let rbody = this.getComponent(cc.RigidBody);
+        // //力
+        // // rbody.applyForce(cc.v2(1000, 0), cc.v2(0, 0), true);
+        // rbody.applyForceToCenter(cc.v2(1000, 0), true);
+        // //速度 每秒多少像素
+        // rbody.linearVelocity = cc.v2(50, 0);
         
     }
 
@@ -52,6 +52,10 @@ export default class BridControl extends cc.Component {
     //结束碰撞
     onEndContact(contact, self, other){
         console.log("结束碰撞");
+    }
+    
+    fly(){
+        this.getComponent(cc.RigidBody).linearVelocity = cc.v2(0, 100);
     }
 
 
